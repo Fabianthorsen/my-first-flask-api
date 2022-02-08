b4119a259a4d552c778a07a77503509ca37df0a2
Fabian Thorsen <thorsen.fabian@gmail.com>
2022-02-08 19:31:59 +0100

    revert changes

--- app.py
+++ app.py
@@ -12,7 +12,9 @@ from db import db
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("URI", "sqlite:///data.db")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+    "DATABASE_URL", "sqlite:///data.db"
+)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["PROPAGATE_EXCEPTIONS"] = True
 app.secret_key = "fabian"
